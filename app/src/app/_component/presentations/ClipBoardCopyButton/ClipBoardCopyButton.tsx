0498fe3e4938133e1ec6ec9aa736a493808ac74e
Takashi Kudo <hazuki3417@gmail.com>
2024-09-27 14:40:35 +0900

    chor: apply code format

--- app/src/app/_component/presentations/ClipBoardCopyButton/ClipBoardCopyButton.tsx
+++ app/src/app/_component/presentations/ClipBoardCopyButton/ClipBoardCopyButton.tsx
@@ -6,31 +6,26 @@ export interface ClipBoardCopyButtonProps {
 	value: string;
 }
 
-export const ClipBoardCopyButton: FC<ClipBoardCopyButtonProps> =
-	(props) => {
-		const { value } = props;
-		return (
-			<CopyButton value={value} timeout={2000}>
-				{({ copied, copy }) => (
-					<Tooltip
-						label={copied ? "Copied" : "Copy"}
-						withArrow
-						position="right"
+export const ClipBoardCopyButton: FC<ClipBoardCopyButtonProps> = (props) => {
+	const { value } = props;
+	return (
+		<CopyButton value={value} timeout={2000}>
+			{({ copied, copy }) => (
+				<Tooltip label={copied ? "Copied" : "Copy"} withArrow position="right">
+					<ActionIcon
+						color={copied ? "teal" : "gray"}
+						variant="subtle"
+						onClick={copy}
 					>
-						<ActionIcon
-							color={copied ? "teal" : "gray"}
-							variant="subtle"
-							onClick={copy}
-						>
-							{copied ? (
-								<IconCheck style={{ width: rem(16) }} />
-							) : (
-								<IconCopy style={{ width: rem(16) }} />
-							)}
-						</ActionIcon>
-					</Tooltip>
-				)}
-			</CopyButton>
-		);
-	};
+						{copied ? (
+							<IconCheck style={{ width: rem(16) }} />
+						) : (
+							<IconCopy style={{ width: rem(16) }} />
+						)}
+					</ActionIcon>
+				</Tooltip>
+			)}
+		</CopyButton>
+	);
+};
 ClipBoardCopyButton.displayName = "component/presentations/ClipBoardCopyButton";
