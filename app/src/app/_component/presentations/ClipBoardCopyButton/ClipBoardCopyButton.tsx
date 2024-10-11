531ff973b77d277c770a529f9ba7717b8fd96faa
hazuki3417 <hazuki3417@gmail.com>
2024-10-11 10:43:38 +0900

    fix: クリップボードにコピーするボタンのレンダリングを最適化

--- app/src/app/_component/presentations/ClipBoardCopyButton/ClipBoardCopyButton.tsx
+++ app/src/app/_component/presentations/ClipBoardCopyButton/ClipBoardCopyButton.tsx
@@ -1,31 +1,46 @@
 import { ActionIcon, CopyButton, Tooltip, rem } from "@mantine/core";
 import { IconCheck, IconCopy } from "@tabler/icons-react";
-import { FC } from "react";
+import { ReactNode, useCallback } from "react";
 
 export interface ClipBoardCopyButtonProps {
 	value: string;
 }
 
-export const ClipBoardCopyButton: FC<ClipBoardCopyButtonProps> = (props) => {
-	const { value } = props;
+type CopyActionIconProps = {
+	copied: boolean;
+	copy: () => void;
+};
+
+const CopyActionIcon = (props: CopyActionIconProps) => {
+	const { copied, copy } = props;
 	return (
-		<CopyButton value={value} timeout={2000}>
-			{({ copied, copy }) => (
-				<Tooltip label={copied ? "Copied" : "Copy"} withArrow position="right">
-					<ActionIcon
-						color={copied ? "teal" : "gray"}
-						variant="subtle"
-						onClick={copy}
-					>
-						{copied ? (
-							<IconCheck style={{ width: rem(16) }} />
-						) : (
-							<IconCopy style={{ width: rem(16) }} />
-						)}
-					</ActionIcon>
-				</Tooltip>
-			)}
-		</CopyButton>
+		<Tooltip label={copied ? "Copied" : "Copy"} withArrow position="right">
+			<ActionIcon
+				color={copied ? "teal" : "gray"}
+				variant="subtle"
+				onClick={copy}
+			>
+				{copied ? (
+					<IconCheck style={{ width: rem(16) }} />
+				) : (
+					<IconCopy style={{ width: rem(16) }} />
+				)}
+			</ActionIcon>
+		</Tooltip>
 	);
+};
+
+const toggleIcon = (payload: {
+	copied: boolean;
+	copy: () => void;
+}): ReactNode => {
+	const { copied, copy } = payload;
+	return <CopyActionIcon copied={copied} copy={copy} />;
+};
+
+export const ClipBoardCopyButton = (props: ClipBoardCopyButtonProps) => {
+	const { value } = props;
+	const memoToggleIcon = useCallback(toggleIcon, []);
+	return <CopyButton value={value} timeout={2000} children={memoToggleIcon} />;
 };
 ClipBoardCopyButton.displayName = "component/presentations/ClipBoardCopyButton";
